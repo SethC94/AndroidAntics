cf21be19d79af81caf5b5256c2e5b372f7cfaaeb
Seth Creasman <sethcreasman@gmail.com>
2017-02-20 02:29:53 -0800

    Final Commit for project specifications

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/AndroidJUnit4.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by BootyCall2.0 on 2/20/2017.
+ */
+public class AndroidJUnit4 {
+}
--- javajokes/src/main/java/com/example/JokeLibrary.java
+++ javajokes/src/main/java/com/example/JokeLibrary.java
@@ -9,8 +9,13 @@ public class JokeLibrary {
         String joke;
         switch (random.nextInt(2)) {
             case 0:
-                joke = "What did one strawberry say to the other? If you weren't so fresh, we wouldn't be in this jam.";
+                joke = "What did one strawberry say to the other?\n If you weren't so fresh, we wouldn't be in this jam.";
                 break;
+            case 1:
+                joke = "What did the pirate say when he turned 80? \n Aye Matey";
+                break;
+
+
             default:
                 joke = "Patient: Doctor! You've got to help me! Whats growing out of my ears!\n" +
                         "Doctor: Well this is Strange. It appears as though corn is growing out of your ears. Why corn?\n" +
